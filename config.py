281ff3c507e13c9dd20b39b9fd126c2f5342b70e
KingPhoenix06 <87867098+KingPhoenix06@users.noreply.github.com>
2023-02-24 21:04:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv('API_ID','3369707'))
+API_HASH = getenv('API_HASH','aec1fd7abdfec322c426961a570ef336')
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID"))
+BOT_TOKEN = getenv('BOT_TOKEN','6148408277:AAHW-0O3LWUby1HLocCQSc2HQj9bRlblQfk')
+OWNER_ID = int(getenv('OWNER_ID','1115053159'))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI")
-MUST_JOIN = getenv("MUST_JOIN", None)
+MONGO_DB_URI = getenv('MONGO_DB_URI','mongodb+srv://amdlinkzz:phoenix06@cluster0.n3s2u8t.mongodb.net/?retryWrites=true&w=majority')
+MUST_JOIN = getenv('MUST_JOIN', 'AMD_LinkZz')
